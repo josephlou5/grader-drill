705f0abbee60436a8e1e329111c4f49d832da5c2
josephlou5 <josephlou@princeton.edu>
2021-07-25 02:11:10 -0700

    Heroku deployment

--- client/src/App/components/questionEditView.jsx
+++ client/src/App/components/questionEditView.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { useHistory } from "react-router-dom";
+import { Link, useHistory } from "react-router-dom";
 import { ResizeTextareas } from "../shared";
 import { getQuestion, updateQuestion } from "../api";
 import QuestionTextField from "./questionTextField";
